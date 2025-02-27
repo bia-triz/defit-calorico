96c2cf4599ea62ce8c825d920dc3f4d0e8d3a777
Beatriz <bia.lucas727@gmail.com>
2025-02-26 21:27:15 -0300

    Tela sem estilos

---
+++ src/screens/home/index.js
@@ -0,0 +1,35 @@
+//1 passo - importar o react
+import React,{use, useState} from "react";
+
+//2 passo - importar os componentes da tela
+import {
+    Text,
+    SafeAreaView,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    StyleSheet,
+    View,
+//    Alert,
+    
+} from 'react-native';
+
+//3 Passo - criar a funçao que cria a tela
+export default function Home(){
+    return(
+    <SafeAreaView>
+        <Text>Défit Calórico</Text>
+
+        <TextInput placeholder="Idade"/>
+
+        <TextInput placeholder="Peso"/>
+
+        <TextInput placeholder="Altura"/>
+       
+
+        <TouchableOpacity>Calcular</TouchableOpacity>
+
+    </SafeAreaView>
+
+    )
+};
